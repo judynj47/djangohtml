80a27b254f6376a7e9e3b2bd5b410fef9b6afb1e
judynj47 <judyswaceke@gmail.com>
2023-07-06 10:42:24 +0300

    first commit

---
+++ morningDjangoHtmlFormsProject/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Product
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def add_product(request):
+    if request.method == "POST":
+        prod_name = request.POST.get("p-name")
+        prod_quantity = request.POST.get("p-qty")
+        prod_size = request.POST.get("p-size")
+        prod_price = request.POST.get("p-price")
+        context = {
+            "prod_name": prod_name,
+            "prod_quantity": prod_quantity,
+            "prod_size": prod_size,
+            "prod_price": prod_price,
+            "success": "Data saved successfully"
+        }
+        query = Product(name=prod_name, qty=prod_quantity,
+                        size=prod_size, price=prod_price)
+        query.save()
+        return render(request, 'add-product.html', context)
+    return render(request, 'add-product.html')
+
