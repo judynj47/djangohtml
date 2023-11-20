add1fad6cdda30cb5af0bbe4e8daa9f1da568406
judynj47 <judyswaceke@gmai.com>
2023-11-20 14:04:59 +0300

    last

--- morningDjangoHtmlFormsProject/settings.py
+++ morningDjangoHtmlFormsProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*hqx!k7h3++0^3&q0ev-r&6fsuli%x)12qu2fhn99un)7mlkbm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
